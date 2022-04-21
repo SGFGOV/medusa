24c5c29ddc283904eb44d92d16fb549db4de71e0
SGF <SGF@sourcegoodfood.com>
2022-04-21 17:25:01 +0000

    packaging

--- packages/medusa-extender-plugin-gstin/dist/modules/address/address.repository.d.ts
+++ packages/medusa-extender-plugin-gstin/dist/modules/address/address.repository.d.ts
@@ -1,4 +1,6 @@
-declare const AddressRepository_base: any;
+import { AddressRepository as MedusaAddressRepository } from '@medusajs/medusa/dist/repositories/address';
+import { Address } from './address.entity';
+declare const AddressRepository_base: import("medusa-extender").MixinReturnType<import("typeorm").Repository<Address>, MedusaAddressRepository>;
 export default class AddressRepository extends AddressRepository_base {
 }
 export {};
