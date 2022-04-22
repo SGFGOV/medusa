3c403071ac0f862ad1e7d217cef422d0467ff59a
SGF <SGF@sourcegoodfood.com>
2022-04-22 04:39:46 +0000

    removing build files

--- packages/medusa-extender-plugin-gstin/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AddressModule = void 0;
-var address_module_1 = require("./modules/address/address.module");
-Object.defineProperty(exports, "AddressModule", { enumerable: true, get: function () { return address_module_1.AddressModule; } });
-//# sourceMappingURL=index.js.map
--- packages/medusa-extender-plugin-gstin/dist/modules/address/address.repository.d.ts
+++
@@ -1,6 +0,0 @@
-import { AddressRepository as MedusaAddressRepository } from '@medusajs/medusa/dist/repositories/address';
-import { Address } from './address.entity';
-declare const AddressRepository_base: import("medusa-extender").MixinReturnType<import("typeorm").Repository<Address>, MedusaAddressRepository>;
-export default class AddressRepository extends AddressRepository_base {
-}
-export {};
