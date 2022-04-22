2b0599317596baea986a23b7219763086ef1b91c
SGF <SGF@sourcegoodfood.com>
2022-04-22 14:34:51 +0000

    updating to validate gst

--- packages/medusa-extender-plugin-gstin/src/modules/address/address.validator.ts
+++ packages/medusa-extender-plugin-gstin/src/modules/address/address.validator.ts
@@ -2,9 +2,19 @@ import { Validator } from 'medusa-extender';
 import { AdminPostOrdersReq } from '@medusajs/medusa/dist/api/routes/admin/orders/create-order'
 import {AddressPayload} from '@medusajs/medusa/dist/types/common'
 import { IsString } from 'class-validator';
+import {isValidGSTNumber} from 'sgf-gstin-validator'
+
+function IsGstin()
+{
+  return function(targetClass: any, propertyKey: string, descriptor?: TypedPropertyDescriptor<() => void>) :  any {
+    return isValidGSTNumber(targetClass[propertyKey])
+   }
+}
 
 @Validator({ override: AddressPayload })
 export default class AddressValidator extends AddressPayload {
+  
+  @IsGstin()
   @IsString()
   gstin: string;
 }
