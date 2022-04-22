5bf800d5596f3f50ca3ea29637140e2cca5503b6
SGF <SGF@sourcegoodfood.com>
2022-04-22 04:39:46 +0000

    removing build files

--- packages/medusa-extender-plugin-gstin/dist/modules/address/address.entity.d.ts
+++
@@ -1,4 +0,0 @@
-import { Address as MedusaAddress } from '@medusajs/medusa';
-export declare class Address extends MedusaAddress {
-    gstin: string;
-}
--- packages/medusa-extender-plugin-gstin/dist/modules/address/address.validator.d.ts
+++
@@ -1,4 +0,0 @@
-import { AddressPayload } from '@medusajs/medusa/dist/types/common';
-export default class AddressValidator extends AddressPayload {
-    gstin: string;
-}
