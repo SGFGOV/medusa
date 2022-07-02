09b549f36384c13860de04fc3aa63ff20c765bd8
SGF <SGF@sourcegoodfood.com>
2022-07-02 01:32:38 +0000

    added support for razorpay and gstin

--- packages/medusa-extender-plugin-gstin/src/modules/address/address.service.ts
+++ packages/medusa-extender-plugin-gstin/src/modules/address/address.service.ts
@@ -1,43 +1,42 @@
-import { Service } from 'medusa-extender';
-import { EntityManager } from 'typeorm';
-import { OrderService as MedusaOrderService } from '@medusajs/medusa/dist/services';
-import EventBusService from '@medusajs/medusa/dist/services/event-bus';
-import AddressRepository from './address.repository';
-import { Address } from './address.entity';
+import { Service } from "medusa-extender"
+import { EntityManager } from "typeorm"
+import { OrderService as MedusaOrderService } from "@medusajs/medusa/dist/services"
+import EventBusService from "@medusajs/medusa/dist/services/event-bus"
+import AddressRepository from "./address.repository"
+import { Address } from "./address.entity"
 
 type ConstructorParams = {
-	loggedInAddress: Address;
-	manager: EntityManager;
-	addressRepository: typeof AddressRepository;
-	eventBusService: EventBusService;
-	orderRepository: any;
-	customerService: any;
-	paymentProviderService: any;
-	shippingOptionService: any;
-	shippingProfileService: any;
-	discountService: any;
-	fulfillmentProviderService: any;
-	fulfillmentService: any;
-	lineItemService: any;
-	totalsService: any;
-	regionService: any;
-	cartService: any;
-	giftCardService: any;
-	draftOrderService: any;
-	inventoryService: any;
+  loggedInAddress: Address
+  manager: EntityManager
+  addressRepository: typeof AddressRepository
+  eventBusService: EventBusService
+  orderRepository: any
+  customerService: any
+  paymentProviderService: any
+  shippingOptionService: any
+  shippingProfileService: any
+  discountService: any
+  fulfillmentProviderService: any
+  fulfillmentService: any
+  lineItemService: any
+  totalsService: any
+  regionService: any
+  cartService: any
+  giftCardService: any
+  draftOrderService: any
+  inventoryService: any
+}
 
-};
-
-@Service({ override: MedusaOrderService, scope: 'SCOPED' })
+@Service({ override: MedusaOrderService, scope: "SCOPED" })
 export default class AddressService extends MedusaOrderService {
-	private readonly manager: EntityManager;
-	private readonly addressRepository: typeof AddressRepository;
-	private readonly eventBus: EventBusService;
+  private readonly manager: EntityManager
+  private readonly addressRepository: typeof AddressRepository
+  private readonly eventBus: EventBusService
 
-	constructor(private readonly container: ConstructorParams) {
-		super(container);
-		this.manager = container.manager;
-		this.addressRepository = container.addressRepository;
-		this.eventBus = container.eventBusService;
-	}
+  constructor(private readonly container: ConstructorParams) {
+    super(container)
+    this.manager = container.manager
+    this.addressRepository = container.addressRepository
+    this.eventBus = container.eventBusService
+  }
 }
