346b69cf057e00944a4ee643e3a4e0cf3178168f
SGF <SGF@sourcegoodfood.com>
2022-07-02 01:32:38 +0000

    added support for razorpay and gstin

--- packages/medusa-extender-plugin-gstin/jest.config.js
+++ packages/medusa-extender-plugin-gstin/jest.config.js
@@ -1,23 +1,12 @@
-module.exports=  {
-	"moduleFileExtensions": [
-		"js",
-		"json",
-		"ts",
-		"d.ts"
-	],
-	"preset": 'ts-jest',
-	"rootDir": "src",
-	"testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
-	"verbose":true,
-	"transformIgnorePatterns": ["/node_modules/"],
-	"collectCoverageFrom": [
-		"**/*.(t|j)s"
-	],
-	"coverageReporters": [
-		"json-summary",
-		"text",
-		"lcov"
-	],
-	"coverageDirectory": "../coverage",
-	"testEnvironment": "node"
-};
+module.exports = {
+  moduleFileExtensions: ["js", "json", "ts", "d.ts"],
+  preset: "ts-jest",
+  rootDir: "src",
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
+  verbose: true,
+  transformIgnorePatterns: ["/node_modules/"],
+  collectCoverageFrom: ["**/*.(t|j)s"],
+  coverageReporters: ["json-summary", "text", "lcov"],
+  coverageDirectory: "../coverage",
+  testEnvironment: "node",
+}
--- packages/medusa-extender-plugin-gstin/src/types/sgf-gstin-validator.d.ts
+++ packages/medusa-extender-plugin-gstin/src/types/sgf-gstin-validator.d.ts
@@ -1,9 +1,19 @@
-declare module "sgf-gstin-validator"
- {
-    export  function isValidGSTNumber(gstin: string): boolean;
-    export  function getGSTINInfo(gstin: string): string;
-    export  function validateEInvoiceSignedQR(qrText: string, publickey: string): string | jwt.JwtPayload;
-    export  function validateSignedInvoice(signedInvoiceJWT: string, publickey: string): string | jwt.JwtPayload;
-    export  function ValidateGSTIN(gstin: string): "Enter a valid 15 character GSTIN" | "Invalid GSTIN format" | "Invalid checksum character in GSTIN" | "Valid GSTIN";
+declare module "sgf-gstin-validator" {
+  export function isValidGSTNumber(gstin: string): boolean
+  export function getGSTINInfo(gstin: string): string
+  export function validateEInvoiceSignedQR(
+    qrText: string,
+    publickey: string
+  ): string | jwt.JwtPayload
+  export function validateSignedInvoice(
+    signedInvoiceJWT: string,
+    publickey: string
+  ): string | jwt.JwtPayload
+  export function ValidateGSTIN(
+    gstin: string
+  ):
+    | "Enter a valid 15 character GSTIN"
+    | "Invalid GSTIN format"
+    | "Invalid checksum character in GSTIN"
+    | "Valid GSTIN"
 }
-
