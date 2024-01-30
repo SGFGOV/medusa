5b7b8148d95aba41defd0d94fdcbe86c9c0d96de
Govind Diwakar <sgf@sourcegoodfood.com>
2024-01-30 10:50:15 +0000

    fix: auth provider parameters updated

--- packages/admin-ui/ui/src/providers/auth-provider.tsx
+++ packages/admin-ui/ui/src/providers/auth-provider.tsx
@@ -7,8 +7,11 @@ export const AuthProvider = ({ children }: PropsWithChildren) => {
   return (
     <Auth0Provider domain={AUTH0_DOMAIN} 
     clientId={AUTH0_CLIENT_ID}
+    cacheLocation={"localstorage"}
+    cookieDomain={`${window.location.hostname ?? "localhost"}`}
+    useCookiesForTransactions={true}
     authorizationParams={{
-      redirect_uri: `${window.location.origin}/app`
+      redirect_uri: `${window.location.origin}/app/login`, 
     }}
 >{children}</Auth0Provider>
 
